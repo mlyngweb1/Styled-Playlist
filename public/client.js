00024b0239a8c775aba0953496f63af46655efe4
mlyngweb1 <31923308+mlyngweb1@users.noreply.github.com>
2018-02-15 11:57:24 +0000

    Initial commit

---
+++ public/client.js
@@ -0,0 +1,15 @@
+$('img').mouseover(function() {
+  $(this).attr('src','assets/beethoven4.jpg');
+});
+
+$('img').mouseout(function() {
+  $(this).attr('src','assets/beethoven3.jpg');
+});
+
+$("#delplaylist").click(function() {
+  return confirm('Really delete this playlist?');
+});
+
+$("#delsong").click(function() {
+  return confirm('Really delete this song?');
+});
